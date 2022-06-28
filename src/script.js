774631def4de952ed6c2704523e7272ccfa261a8
regmis <sandeshreg35@gmail.com>
2022-06-27 20:49:54 -0500

    testing adding event listener

---
+++ src/script.js
@@ -0,0 +1,9 @@
+let list = document.getElementById("list");
+let newTask = document.getElementById("newTask");
+
+document.getElementById("addTask").addEventListener('click', () => {
+    // list.insertAdjacentHTML('beforeend', `<li class="list-group-item">${newTask.value}</li>`);
+    // console.log("button clicked, list added");
+    // newTask.value = '';
+    console.log("button clicked");
+});
